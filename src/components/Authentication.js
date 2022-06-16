5c8eb2de898291982bfbf9f6a9a795f9d818bc25
Chandra shekar <80117733+chandra-sekharan@users.noreply.github.com>
2022-06-16 18:42:03 +0530

    Update Authentication.js

--- src/components/Authentication.js
+++ src/components/Authentication.js
@@ -2,7 +2,7 @@
 
 
 const CLIENT_ID = "b45ff7c51742414ca1581a5a8e91e89f";
-const REDIRECT_URI = "http://localhost:3000/";
+const REDIRECT_URI = "https://spotify-react-cs.herokuapp.com/";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token"
 
